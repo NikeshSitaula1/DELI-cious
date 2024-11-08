4a1c1f4610fc42039a64acddb5058ed37bcaa87f
Nikesh Sitaula <sitaulanike@gmail.com>
2024-11-08 16:28:11 -0500

    override price of chips

--- src/main/java/com/pluralsight/items/Chips.java
+++ src/main/java/com/pluralsight/items/Chips.java
@@ -7,22 +7,38 @@ import java.util.List;
 
 public class Chips extends Items {
 
-    private String chipType;
+    private String chipsType;
+    private final double chipsSmall = 1.5;
+    private final double chipsMedium = 2.0;
+    private final double chipsLarge = 2.5;
+
 
     public Chips(String chipType) {
-        this.chipType = chipType;
+        this.chipsType = chipType;
     }
 
     public String getChipType() {
-        return chipType;
+        return chipsType;
     }
 
     public void setChipType(String chipType) {
-        this.chipType = chipType;
+        this.chipsType = chipType;
     }
 
     @Override
     public double totalPrice() {
+        if (chipsType.equalsIgnoreCase("Small")) {
+            return chipsSmall;
+        }
+        else if(chipsType.equalsIgnoreCase("Medium")) {
+            return chipsMedium;
+        }
+        else if (chipsType.equalsIgnoreCase("Large")) {
+            return chipsLarge;
+        }
+        else {
+            System.out.println("Please choose between Small, Medium or Large. ");
+        }
         return 0;
     }
 }
