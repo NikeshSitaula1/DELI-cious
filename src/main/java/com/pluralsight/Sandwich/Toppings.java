a330b2ee5985f64e6a658fe6f09c56ae6622f79e
Nikesh Sitaula <sitaulanike@gmail.com>
2024-11-09 10:54:52 -0500

    Connecting Toppings with Sandwich not done

--- src/main/java/com/pluralsight/Sandwich/Toppings.java
+++ src/main/java/com/pluralsight/Sandwich/Toppings.java
@@ -1,7 +1,45 @@
 package com.pluralsight.Sandwich;
 
+import com.pluralsight.items.Sandwich;
+
 public class Toppings {
 
+    private String toppingName;
+    private boolean toppingIsPremium;
+    private double toppingExtra;
 
+    public Toppings(String toppingName, boolean toppingIsPremium, double toppingExtra) {
+        this.toppingName = toppingName;
+        this.toppingIsPremium = toppingIsPremium;
+        this.toppingExtra = toppingExtra;
+    }
 
+    public String getToppingName() {
+        return toppingName;
+    }
+
+    public void setToppingName(String toppingName) {
+        this.toppingName = toppingName;
+    }
+
+    public boolean isToppingIsPremium() {
+        return toppingIsPremium;
+    }
+
+    public void setToppingIsPremium(boolean toppingIsPremium) {
+        this.toppingIsPremium = toppingIsPremium;
+    }
+
+    public double getToppingExtra() {
+        return toppingExtra;
+    }
+
+    public void setToppingExtra(double toppingExtra) {
+        this.toppingExtra = toppingExtra;
+    }
+
+    public double calculateCost() {
+
+        return 0;
+    }
 }
--- src/main/java/com/pluralsight/items/Sandwich.java
+++ src/main/java/com/pluralsight/items/Sandwich.java
@@ -1,8 +1,47 @@
 package com.pluralsight.items;
 
+import com.pluralsight.Sandwich.Toppings;
 import com.pluralsight.checkout.Items;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sandwich extends Items {
+
+    private String sandwichSize;
+    private String sandwichBread;
+    private List<Toppings> toppingsList =  new ArrayList<Toppings>();
+
+    public Sandwich(String sandwichSize, String sandwichBread) {
+        this.sandwichSize = sandwichSize;
+        this.sandwichBread = sandwichBread;
+    }
+
+    public Sandwich(){}
+
+    public String getSandwichSize() {
+        return sandwichSize;
+    }
+
+    public void setSandwichSize(String sandwichSize) {
+        this.sandwichSize = sandwichSize;
+    }
+
+    public String getSandwichBread() {
+        return sandwichBread;
+    }
+
+    public void setSandwichBread(String sandwichBread) {
+        this.sandwichBread = sandwichBread;
+    }
+
+    public List<Toppings> getToppingsList() {
+        return toppingsList;
+    }
+
+    public void setToppingsList(List<Toppings> toppingsList) {
+        this.toppingsList = toppingsList;
+    }
 
 
 
